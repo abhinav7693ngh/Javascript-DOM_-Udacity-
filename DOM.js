61d976092839b6b3d27e7db30fc6dc395c01f187
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-11 13:12:08 +0530

    Event Capturing and Bubbling

--- DOM.js
+++ DOM.js
@@ -11,4 +11,23 @@
 // console.log(document.querySelector('body').lastChild);
 
 
-console.log(document.getElementById('my').textContent);
+// const my = document.createElement('span');
+// my.textContent ='!!!';
+
+
+// var first = document.querySelector('.para');
+// var second = document.querySelector('#para');
+// first.appendChild(my);
+// second.appendChild(my);
+
+document.querySelector('body').addEventListener('click',(event) => {
+    console.log('Listener of body tag : bubbling ', event.target);
+});
+
+document.querySelector('.test').addEventListener('click', (event) => {
+    console.log('Listener of paragraph tag : capturing ', event.target);
+}, true);
+
+document.getElementById('mybtn').addEventListener('click',(event)=>{
+    console.log('Listener of target button : bubbling ', event.target);
+});
