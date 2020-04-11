d932ab509cc813bb6926e736b3b79f97362de75c
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-11 08:30:59 +0530

    first commit

---
+++ DOM.js
@@ -0,0 +1,14 @@
+// //console.log(Array.from(document.querySelectorAll('div.test .my'))[0].parentNode);
+
+// var oldchild = document.getElementById('para');
+
+// var mynew = document.createElement('h1');
+// mynew.innerHTML = 'This is my new replaced element';
+// mynew.id = 'new';
+
+// console.log(document.querySelector('.test').classList.value);
+
+// console.log(document.querySelector('body').lastChild);
+
+
+console.log(document.getElementById('my').textContent);
